ac55a034601b5e233b23720603bfc4505a61c849
Sadab Masrur <sadabmasrur08@gmail.com>
2025-01-21 15:29:41 +0600

    h

---
+++ squareacp.py
@@ -0,0 +1,21 @@
+
+import turtle
+
+turtle.Screen().bgcolor("cyan")
+
+star=turtle.Turtle()
+
+star.forward(200)
+
+star.left(90)
+star.forward(200)
+
+star.left(90)
+star.forward(200)
+
+star.left(90)
+star.forward(200)
+
+
+
+turtle.done()
